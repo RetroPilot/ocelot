eab968d42d4acb4940764fc4b5987cbf5245deb2
Kevin Roscom <37704329+wocsor@users.noreply.github.com>
2025-04-28 14:00:11 -0600

    make nice, update readme

--- firmware/clean_docker.sh
+++ firmware/clean_docker.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Remove containers with label
-echo "Removing containers with label 'project=ocelot'..."
+# Remove containers with label ocelot
 docker ps -a --filter "label=project=ocelot" -q | xargs -r docker rm -f
-
-# Remove images with label
-echo "Removing images with label 'project=ocelot'..."
 docker images --filter "label=project=ocelot" --format "{{.ID}}" | xargs -r docker rmi -f
-
-echo "Label-based cleanup complete!"
--- firmware/flash_docker.sh
+++ firmware/flash_docker.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 set -e
 
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <project>"
-    exit 1
-fi
+# if [ $# -eq 0 ]; then
+#     echo "Usage: $0 <project>"
+#     exit 1
+# fi
 
 docker run --rm --device /dev/bus/usb --privileged  ocelot-builder $1
